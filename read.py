5e2cc0026564350eedddf65d021032376ffa5cba
alan YU <alan19951024@gmail.com>
2020-01-14 16:43:45 +0800

    add verson 2

--- read.py
+++ read.py
@@ -6,3 +6,10 @@ with open('reviews.txt' , 'r') as f:
 		count += 1 #count= count+1
 		if count % 1000 == 0:
 			print(len(data))
+
+print('檔案讀取玩了 , 總共有' , len(data),'筆資料' )
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d) 
+print ('平均留言長度是', sum_len / len(data))
